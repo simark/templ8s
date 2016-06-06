cbad9cfda99a1e7b70dcdbd9448611ceae627b0b
Simon Marchi <simon.marchi@polymtl.ca>
2016-06-06 11:00:37 -0400

    Add barrier to wait until threads are started

--- c/threads.c
+++ c/threads.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <pthread.h>
 
+static pthread_barrier_t barrier;
+
 void *t1_func(void *arg) {
+	int i;
+
+	pthread_barrier_wait (&barrier);
+
+	for (i = 0; i < 100; i++) {
+		sleep (1);
+	}
+
 	return NULL;
 }
 
 void *t2_func(void *arg) {
+	int i;
+
+	pthread_barrier_wait (&barrier);
+
+	for (i = 0; i < 100; i++) {
+		sleep (1);
+	}
+
 	return NULL;
+}
+
+void all_threads_started (void)
+{
 }
 
 int main(int argc, char **argv) {
 	pthread_t t1, t2;
 
+	pthread_barrier_init (&barrier, NULL, 3);
+
 	pthread_create(&t1, NULL, t1_func, NULL);
 	pthread_create(&t2, NULL, t2_func, NULL);
+
+	pthread_barrier_wait (&barrier);
+
+	all_threads_started ();
 
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
